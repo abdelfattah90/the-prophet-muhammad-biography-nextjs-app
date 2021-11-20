2a77b5a8d6303978478f6e8067d0e1e9bfdfe61f
unknown <ahmedomar1007@gmail.com>
2021-11-20 16:38:28 +0200

    change 3 main color

--- components/HeadComponent.js
+++ components/HeadComponent.js
@@ -8,7 +8,7 @@ export default function HeadComponent(props) {
       <link rel='apple-touch-icon' href='/favicon.ico' />
       <meta charset='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta name='theme-color' content='#537bc4' />
+      <meta name='theme-color' content='#a99260' />
       <meta name='description' content='السيرة النبوية للدكتور على الصلابى' />
       <meta name='robots' content='index,follow' />
       <meta name='googlebot' content='index,follow' />
